6bff7e9f0c64950f4a06cb81677900628054f033
Ben Baert <benbaert246@gmail.com>
2022-09-29 19:14:42 +0200

    bug: fix vpc connector egress settings validation

--- variabes.tf
+++ variabes.tf
@@ -145,8 +145,11 @@ EOD
 variable "vpc_connector_egress_settings" {
   type = string
   validation {
-    condition = (var.vpc_connector_egress_settings == null ? false : (
-      contains(["ALL_TRAFFIC", "PRIVATE_RANGES_ONLY"], var.vpc_connector_egress_settings)))
+    condition = (
+      var.vpc_connector_egress_settings == null ? true : (
+        contains(["ALL_TRAFFIC", "PRIVATE_RANGES_ONLY"], var.vpc_connector_egress_settings)
+      )
+    )
     error_message = "Must be one of ALL_TRAFFIC, PRIVATE_RANGES_ONLY"
   }
   description = <<EOD
